7c6e2a6868e896c412d0fdb1716a6e81b0ac478a
CJK2016 <2573184601@qq.com>
2018-05-08 09:40:37 +0800

    1

---
+++ service-order/src/main/java/com/itmayiedu/controller/OrderController.java
@@ -0,0 +1,23 @@
+package com.itmayiedu.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.itmayiedu.service.OrderMemberService;
+
+@RestController
+public class OrderController {
+	@Autowired
+	private OrderMemberService orderMemberService;
+
+	@RequestMapping("/getOrderUserAll")
+	public List<String> getOrderUserAll() {
+		System.out.println("订单服务开始调用会员服务");
+		return orderMemberService.getOrderUserAll();
+
+	}
+
+}
